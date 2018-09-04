ca81aafa9e0b7061caae500b7ed863abfe623d53
MarsXq <776356314@qq.com>
2018-09-04 10:34:25 +0800

    资源文件加载优化

--- app/src/main/java/com/example/administrator/mybitmapsize/ThirdActivity.java
+++ app/src/main/java/com/example/administrator/mybitmapsize/ThirdActivity.java
@@ -1,18 +1,24 @@
 package com.example.administrator.mybitmapsize;
 
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.TypedValue;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.example.administrator.mybitmapsize.util.BitmapUtils;
 
 
 public class ThirdActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ImageView imageView1;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -22,12 +28,20 @@ public class ThirdActivity extends AppCompatActivity implements View.OnClickList
         TextView textView1 = (TextView) this.findViewById(R.id.tv1_third);
         TextView textView2 = (TextView) this.findViewById(R.id.tv2_third);
         TextView textView3 = (TextView) this.findViewById(R.id.tv3_third);
-        ImageView imageView1 = (ImageView) this.findViewById(R.id.iv1_third);
+        imageView1 = (ImageView) this.findViewById(R.id.iv1_third);
 
         textView1.setOnClickListener(this);
         textView2.setOnClickListener(this);
         textView3.setOnClickListener(this);
 
+        Runtime runtime = Runtime.getRuntime();
+        long maxMemory = runtime.maxMemory();
+        long freeMemory = runtime.freeMemory();
+        long totalMemory = runtime.totalMemory();
+        System.out.println("maxMemory======================" + (maxMemory / 1024 / 1024) + " Mb");
+        System.out.println("freeMemory======================" + (freeMemory / 1024 / 1024) + " Mb");
+        System.out.println("totalMemory======================" + (totalMemory / 1024 / 1024) + " Mb");
+        System.out.println("已使用======================" + ((totalMemory - freeMemory) / 1024 / 1024) + " Mb");
     }
 
     @Override
@@ -64,6 +78,58 @@ public class ThirdActivity extends AppCompatActivity implements View.OnClickList
                 // 1242*1786*3/1024/1024=6.346M
             }
             break;
+
+            case R.id.tv3_third:
+                //setImageResource/setBackgroundResource/setImageBitmap/直接xml设置src/直接xml设置background：设置图片所占内存与设置的大小无关，且强制回收回收不掉
+                //xml直接设置比set方法所占内存少
+//setImageResource:占用内存如下：
+//                20dp*20dp 占用55.34MB
+//                200dp*200dp 占用55.39MB
+//                match_parent 占用56.1MB
+//                所以设置图片src占用内存与设置尺寸无关
+//setBackgroundResource:占用内存如下：
+//                20dp*20dp 占用56.33MB
+//                200dp*200dp 占用55.37MB
+//                match_parent 占用55.36MB
+//setImageBitmap:占用内存如下：
+//                20dp*20dp 占用55.35MB
+//                200dp*200dp 占用55.34MB
+//                match_parent 占用55.43MB
+//直接设置src：
+//                20dp*20dp 占用49.91MB
+//                200dp*200dp 占用49.95MB
+//                match_parent 占用49.89MB
+//直接设置background：
+//                20dp*20dp 占用49.79MB
+//                200dp*200dp 占用49.91MB
+//                match_parent 占用49.83MB
+//glide设置图片：
+//                20dp*20dp 占用49.89MB
+//                200dp*200dp 占用49.98MB
+//                match_parent 占用55.88MB
+            {
+//                imageView1.setImageResource(R.drawable.start_pic_xx);
+
+//                imageView1.setBackgroundResource(R.drawable.start_pic_xx);
+
+//                Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.start_pic_xx);
+//                imageView1.setImageBitmap(bitmap);
+
+                Glide.with(this)
+                        .load(R.drawable.start_pic_xx)
+                        .diskCacheStrategy(DiskCacheStrategy.NONE)
+                        .skipMemoryCache(true)
+                        .dontAnimate()
+                        .crossFade()
+//                        .override(((int) dp2px(200)), ((int) dp2px(200)))//相当于压缩图片，从像素（占用内存并未减少，不管20,200还是其他）
+                        .into(imageView1);
+            }
+            break;
         }
+    }
+
+    private static float dp2px(float dp) {
+        Resources r = Resources.getSystem();
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, r.getDisplayMetrics());
     }
 }
